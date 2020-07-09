fc8deaf3b46ca25d5f690ab78d0ec5719d87e662
Gustavo Escobar <gustavex12345@gmail.com>
2020-07-09 17:59:06 -0500

    ENTIDAD ESTUDIANTE LISTA

--- app/src/main/java/upn/edu/pe/huanchacolibraryapp/models/entity/Estudiante.kt
+++ app/src/main/java/upn/edu/pe/huanchacolibraryapp/models/entity/Estudiante.kt
@@ -1,5 +1,34 @@
 package upn.edu.pe.huanchacolibraryapp.models.entity
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
 class Estudiante {
-    GAAAAAAAA
+    @SerializedName("id")
+    @Expose
+    var id= 0
+
+    @SerializedName("nombre")
+    @Expose
+    var nombres: String? = null
+
+    @SerializedName ("apellidos")
+    @Expose
+    var apellidos: String? = null
+
+    @SerializedName("carrera")
+    @Expose
+    var carrera: String? = null
+
+    @SerializedName("email")
+    @Expose
+    var email: String? = null
+
+    constructor(id: Int, apellidos:String?, nombres: String?, carrera: String?, email:String?){
+        this.id= id
+        this.apellidos=apellidos
+        this.nombres=nombres
+        this.carrera=carrera
+        this.email=email
+    }
 }
--- app/src/main/java/upn/edu/pe/huanchacolibraryapp/models/entity/Libro.kt
+++ app/src/main/java/upn/edu/pe/huanchacolibraryapp/models/entity/Libro.kt
@@ -1,4 +1,27 @@
 package upn.edu.pe.huanchacolibraryapp.models.entity
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
 class Libro {
+    @SerializedName("id_Libro")
+    @Expose
+    var idLibro =0
+
+    @SerializedName("nombre_Libro")
+    @Expose
+    var nombreLibro: String? = null
+
+    @SerializedName("editorial_Libro")
+    @Expose
+    var editorialLibro: String? = null
+
+    @SerializedName("categoria_Libro")
+    @Expose
+    var categoriaLibro: String? = null
+
+    @SerializedName("ejemplar_Libro")
+    @Expose
+    var ejemplarLibro: String? =null
+
 }
