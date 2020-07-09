a4803162723be6b122b03482a5e9fdf4b59b5eac
Gustavo Escobar <gustavex12345@gmail.com>
2020-07-09 18:58:06 -0500

    RESPONSEAPI ESTUDIANTE

--- app/src/main/java/upn/edu/pe/huanchacolibraryapp/services/ServiceEstudiante/ResponseApiServiceEstudiante.java
+++
@@ -1,4 +0,0 @@
-package upn.edu.pe.huanchacolibraryapp.services.ServiceEstudiante;
-
-public class ResponseApiServiceEstudiante {
-}
---
+++ app/src/main/java/upn/edu/pe/huanchacolibraryapp/utils/ApiUtils.kt
@@ -0,0 +1,5 @@
+package upn.edu.pe.huanchacolibraryapp.utils
+
+object ApiUtils {
+    const val BASE_URL ="http://192.168.0.2:8080/api/estudiantes"//CAMBIAR
+}
