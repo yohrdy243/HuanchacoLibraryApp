5cdb3902203a886505d0797d990177bd5303b091
Gustavo Escobar <gustavex12345@gmail.com>
2020-07-10 00:09:14 -0500

    TODOS LOS ADAPTADORES y LISTAR

--- app/src/main/java/upn/edu/pe/huanchacolibraryapp/utils/ApiUtils.kt
+++ app/src/main/java/upn/edu/pe/huanchacolibraryapp/utils/ApiUtils.kt
@@ -2,4 +2,6 @@ package upn.edu.pe.huanchacolibraryapp.utils
 
 object ApiUtils {
     const val BASE_URL ="http://192.168.0.2:8080/api/estudiantes"//CAMBIAR
+    const val BASE_URL1 ="http://192.168.0.2:8080/api/libros"//CAMBIAR
+    const val BASE_URL2 ="http://192.168.0.2:8080/api/reservas"//CAMBIAR
 }
